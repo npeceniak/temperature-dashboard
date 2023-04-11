07af490f264f4b1f1a8477fc9ce65c3795f3e0da
Nick Peceniak <npeceniak@magnite.com>
2023-04-11 17:26:41 -0600

    Add Daily outdoor temp and update date format used

--- src/chart/chart.ts
+++ src/chart/chart.ts
@@ -23,7 +23,7 @@ export function renderMultiDayChart(jsonResponse) {
 
         const data = newarray.map(element => {
             return {
-                x: element.timestamp,
+                x: getRoundedDate(5, new Date(element.timestamp)).toLocaleString(),
                 y: parseInt(element.temperature)
             }
         })
@@ -59,6 +59,12 @@ export function addChartToPage(endpoint) {
         .then(data => renderChart(data));
 }
 
+export function addOutdoorChartToPage(endpoint) {
+    fetch(endpoint)
+        .then(response => response.json())
+        .then(data => renderOutdoorTemp(data));
+}
+
 function renderChart(inputData) {
     const last24Hours = inputData.history;
     const title = inputData.data.name;
@@ -69,7 +75,7 @@ function renderChart(inputData) {
 
     const temperature = last24Hours.map(element => {
         return {
-            x: element.timestamp.substring(11),
+            x: getRoundedDate(5, new Date(element.timestamp)).toLocaleString(),
             y: parseInt(element.temperature)
         }
     })
@@ -90,3 +96,59 @@ function renderChart(inputData) {
 
     new Chart(ctx, config)
 }
+
+export function renderOutdoorTemp(outdoorData){
+    // let temperatureOutdoor = []
+    let title = "Outdoor Temperature";
+    const now = new Date(Date.now())
+
+    console.log(outdoorData);
+
+    const hourlyData = outdoorData.hourly;
+
+    const filterForcastData = outdoorData.hourly.time.filter((element) => {
+        const time = new Date(element);
+        return time < now
+    });
+
+    const temperatureOutdoor = filterForcastData.map((element, index) => {
+        console.log("Filtered:", element, index)
+        const graphData = {
+            x: new Date(element).toLocaleString(),
+            y: hourlyData.temperature_2m[index]
+        }
+        return graphData;
+    })
+
+    const chartContainer = new ChartContainer();
+    chartContainer.buildChartContainer(title)
+
+    const ctx = chartContainer.chartCanvasElement;
+
+    const config: any = {
+        type: 'line',
+        data: {
+            datasets: [
+                {
+                    label: 'Outdoor Temperature',
+                    data: temperatureOutdoor.slice(-24), //Only show last 24 hours of data.
+                    fill: false,
+                    borderColor: 'rgb(0, 0, 0)'
+                }
+            ]
+        }
+    }
+
+    new Chart(ctx, config)
+}
+
+function getRoundedDate(minutes, date) {
+    console.log("Input Date", date)
+
+    let ms = 1000 * 60 * minutes; // convert minutes to ms
+    let roundedDate = new Date(Math.floor(date.getTime() / ms) * ms);
+  
+    console.log("Rounded Date", roundedDate)
+    return roundedDate
+}
+
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
-import { addChartToPage } from './chart/chart';
+import { addChartToPage, addOutdoorChartToPage } from './chart/chart';
+
+addOutdoorChartToPage("https://api.open-meteo.com/v1/forecast?latitude=39.61&longitude=-105.11&hourly=temperature_2m&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&past_days=1&forecast_days=1&timezone=America%2FDenver")
 
 //Add Default Charts...
 addChartToPage("http://office.temp.lan/data")
@@ -6,3 +8,4 @@ addChartToPage("http://basement.temp.lan/data")
 addChartToPage("http://livingroom.temp.lan/data")
 addChartToPage("http://garage.temp.lan/data")
 addChartToPage("http://upstairs.temp.lan/data")
+addChartToPage("http://furnace.temp.lan/data")
